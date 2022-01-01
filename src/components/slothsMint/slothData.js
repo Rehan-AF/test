0fee1df8c0df2c8fff33cc0f087429d759665fee
Rehan-AF <af.rehan03@gmail.com>
2022-01-01 18:19:31 +0500

    counter design

---
+++ src/components/slothsMint/slothData.js
@@ -0,0 +1,24 @@
+import img1 from "../../assets/mint/1.gif";
+import img2 from "../../assets/mint/2.gif";
+import img3 from "../../assets/mint/3.gif";
+import img4 from "../../assets/mint/4.gif";
+
+const SlothData = [
+  {
+    img: img1,
+    alt: "gif",
+  },
+  {
+    img: img2,
+    alt: "gif",
+  },
+  {
+    img: img3,
+    alt: "gif",
+  },
+  {
+    img: img4,
+    alt: "gif",
+  },
+];
+export default SlothData;
